63ede6a5bcf6b1abbc0ce090f6252bf4acf6e141
dhacker29 <davidhackerdvm@gmail.com>
2012-08-27 21:57:26 -0500

    move non device specific items to vendor common

--- umts_spyder-vendor.mk
+++ umts_spyder-vendor.mk
@@ -12,20 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Live wallpaper packages
-PRODUCT_PACKAGES := \
-    LiveWallpapers \
-    LiveWallpapersPicker \
-    MagicSmokeWallpapers \
-    VisualizationWallpapers \
-    librs_jni
-
-# Publish that we support the live wallpaper feature.
-PRODUCT_COPY_FILES := \
-    packages/wallpapers/LivePicker/android.software.live_wallpaper.xml:/system/etc/permissions/android.software.live_wallpaper.xml
-
 # Pick up overlay for features that depend on non-open-source files
 #DEVICE_PACKAGE_OVERLAYS := vendor/motorola/umts_spyder/overlay
 
 $(call inherit-product, vendor/motorola/umts_spyder/umts_spyder-vendor-blobs.mk)
-
+$(call inherit-product, vendor/motorola/common/common-vendor.mk)
