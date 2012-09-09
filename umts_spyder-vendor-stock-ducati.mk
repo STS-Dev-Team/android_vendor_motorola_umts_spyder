fb4cdad2cff22240948810aaf4cf74b24bb45bad
dhacker29 <davidhackerdvm@gmail.com>
2012-09-09 13:35:32 -0500

    Allow kexec and stock-based to build from same device/vendor

---
+++ umts_spyder-vendor-stock-ducati.mk
@@ -0,0 +1,6 @@
+VENDOR_BLOB_FOLDER := vendor/motorola/umts_spyder/proprietary
+
+# system/etc
+PRODUCT_COPY_FILES += \
+$(VENDOR_BLOB_FOLDER)/etc/firmware/ducati-m3.bin:/system/etc/firmware/ducati-m3.bin
+
